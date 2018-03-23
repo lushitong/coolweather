51d4f28525fb1afd827c407108d84c83c6bba960
lstong <lu_shitong@163.com>
2018-03-23 13:27:14 +0800

    新增记住密码功能,新增登录功能,version1.0.323.3

--- app/src/main/java/com/coolweather/android/LoginActivity.java
+++ app/src/main/java/com/coolweather/android/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.coolweather.android;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
@@ -13,42 +15,79 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText account_edit;
-    private TextView password_edit;
-    private CheckBox checkbox;
+    private EditText password_edit;
+    private CheckBox remember_password;
+    private CheckBox show_password;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        account_edit=(EditText)findViewById(R.id.account_edit);//登录账号
+        password_edit=(EditText) findViewById(R.id.password_edit);//登录密码
+        show_password=(CheckBox)findViewById(R.id.show_password);//显示密码复选框
+        remember_password=(CheckBox)findViewById(R.id.remeber_password); //记住密码复选框
+        Button login_button = (Button) findViewById(R.id.login_button);//登录button
+        Button reg_button=(Button)findViewById(R.id.reg_button);//注册button
+        preferences= PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isRemember=preferences.getBoolean("remember_password",false);
 
-        password_edit=(TextView)findViewById(R.id.password_edit);
-        checkbox=(CheckBox)findViewById(R.id.checkbox);
-        checkbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+        //将账号和密码都设置到文本框中
+        if (isRemember){
+            String account=preferences.getString("account_edit","");
+            String password=preferences.getString("password_edit","");
+            account_edit.setText(account);
+            password_edit.setText(password);
+            remember_password.setChecked(true);
+        }
+
+        //设置密码是否隐藏
+        show_password.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked){
-                    password_edit.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    password_edit.setTransformationMethod(HideReturnsTransformationMethod.getInstance());//如果选中，显示密码
                 }
-                else password_edit.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                else password_edit.setTransformationMethod(PasswordTransformationMethod.getInstance());//否则隐藏密码
+
             }
         });
 
-                //登录按键
-                Button login_button = (Button) findViewById(R.id.login_button);
+        //登录后跳转的Activity
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
+                String account=account_edit.getText().toString();
+                String password=password_edit.getText().toString();
+                if (account.equals("lushitong")&&password.equals("123456")) {
+                    editor=preferences.edit();
+                    if (remember_password.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }
+                    else {
+                        editor.clear();
+                    }
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                   // finish();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"账号或者密码错误",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-        //注册按键,点击后会调用浏览器打开指定网址,
-        Button reg_button=(Button)findViewById(R.id.reg_button);
+        //注册按键,点击后会调用浏览器打开指定网址
         reg_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
